13b61fcd9307369a7b6fce0a9032e9eca3d604c3
Ahmed Adnan <48983632+adnan23062000@users.noreply.github.com>
2021-08-24 06:24:38 +0600

    Add files via upload

---
+++ Draft/sample/ClassRoutine.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class ClassRoutine {
+
+    public void RoutineAccordingToClass(String[] ara) throws FileNotFoundException {
+        File file = new File("src\\classRoutine");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+
+            String[] elements = scanner.nextLine().split("->", 0);
+            int dayToday = Integer.parseInt(elements[0]);
+            String sub1 = elements[1];
+            String sub2 = elements[2];
+            String sub3 = elements[3];
+
+            LocalDate date = LocalDate.now();
+            DayOfWeek day = DayOfWeek.from(date);
+
+            int val = day.getValue();
+
+            //Routine obj = new Routine();
+
+            if (val == dayToday) {
+                ara[0] = elements[1];
+                ara[1] = elements[2];
+                ara[2] = elements[3];
+            }
+        }
+    }
+}
